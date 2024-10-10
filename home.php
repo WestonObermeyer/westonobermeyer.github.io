88e7b2cb221e7a5e3247f842d7a7998fed47c124
Weston <144749173+WestonObermeyer@users.noreply.github.com>
2024-10-10 15:15:13 -0600

    Update and rename home.html to home.php

--- home.php
+++ home.php
@@ -1,3 +1,16 @@
+<?php
+
+$texts = [ 
+    "Hello, world!", 
+    "Welcome to my website!", 
+    "Enjoy your stay!", 
+    "Random quote of the day!", 
+    "Have a great day!" 
+];
+
+$randomText = $texts[array_rand($texts)]; 
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
